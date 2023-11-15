e79cab6230e6e3839bdd37438e49117f5632ac6a
Dora <dora.sparagno@gmail.com>
2023-11-15 18:20:02 +0100

    JS (implemented new functions)

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,67 +7,87 @@ function getDisplay(displayCounter){
     return numberCount;
 }
 
-function addCount(counter){
+
+
+function addNumber(counter){
     counter = counter+1;
     return counter;
 }
 
-function subtractCount(counter){
+function subtractNumber(counter){
     counter = counter-1;
     return counter;
 }
 
-// function updateCounter(value){
-
-    
-// }
+function updateCounter(op){
+    let number = getDisplay(displayCounter);
+    if (op===plus){
+        newValue = addNumber(number);
+    }else {
+        newValue = subtractNumber(number);
+    };
+    displayCounter.textContent= newValue;
+}
 
 
 //DOM
 
 const displayCounter = document.getElementById("value-counter");
-
-
-
 const btnContainer = document.querySelector(".btn-container");
-const buttons = btnContainer.querySelectorAll(".btn");
-// for (let button of buttons){
-//     console.log(button.getAttribute("id"));
-
-// };
-
-// buttons.forEach(button=> {
-//     console.log(button.getAttribute("id"));
-// });
 const plus = document.getElementById("plus-btn");
-
 const minus = document.getElementById("minus-btn");
- 
+
 
 // Event
 
-// buttons.addEventListener("click",()=>{
-//     let flag = true;
-//     while (flag===true){
 
-//     }
+// plus.addEventListener("click", ()=>{
+//     let number = getDisplay(displayCounter);
+//     newValue = addNumber(number);
+//     displayCounter.textContent= newValue;
+//     console.log(newValue);
+// })
+// minus.addEventListener("click", ()=>{
+//     let number = getDisplay(displayCounter);
+//     newValue = subtractNumber(number);
+//     displayCounter.textContent= newValue;
+//     console.log(newValue);
 // })
 
 
- plus.addEventListener("click", ()=>{
-     let number = getDisplay(displayCounter);
-     newValue = addCount(number);
-     displayCounter.textContent= newValue;
-     console.log(newValue);
- })
- minus.addEventListener("click", ()=>{
-     let number = getDisplay(displayCounter);
-     newValue = subtractCount(number);
-     displayCounter.textContent= newValue;
-     console.log(newValue);
- })
+
+//Scrivo il procedimento: 1. prendi il numero che segna il display, 2 riconosci il tipo di bottone premuto, 3 aggiungi un evento al bottone selezionato, 4 applica la funzione di
+//addizione o sottrazione 5. cambia il numero sul display ---> problema 2 ho un container che ha due bottoni come faccio a capire quale dei due ho premuto ?
 
 
 
+// btnContainer.addEventListener("click", (e)=>{
+//     let operation = e.target.closest("button");
+//     if (operation!=null){
+//         if(operation===plus){
+//             let number = getDisplay(displayCounter);
+//             newValue = addNumber(number);
+//             displayCounter.textContent= newValue;             
+//         }else{
+//             let number = getDisplay(displayCounter);
+//             newValue = subtractNumber(number);
+//             displayCounter.textContent= newValue;
+//         };
+//     };
+
+// }) 
+
+
+    
+
+
+
+
+btnContainer.addEventListener("click", (e)=>{
+    let operation = e.target.closest("button");
+    if (operation!=null){
+        updateCounter(operation);
+    };
+}) 
 
 
