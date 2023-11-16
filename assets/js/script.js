422975bba44bf2b74938aa33f23790612a9be26f
Dora <dora.sparagno@gmail.com>
2023-11-16 16:56:59 +0100

    feat: Finished js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,29 @@
 //functions
 
-function getDisplay(displayCounter){
-    let count = displayCounter.textContent;
+function getDisplay(display){
+    let count = display.textContent;
     let numberCount = parseInt(count);
-    console.log(numberCount);
     return numberCount;
 }
 
 
-
 function addNumber(counter){
-    counter = counter+1;
+    counter++;
     return counter;
 }
 
 function subtractNumber(counter){
-    counter = counter-1;
+    counter--;
     return counter;
+}
+
+function changeColor(number, display){
+    if (number%2 ===0){
+        display.style.color="#fdf0d5";
+    }else{
+        display.style.color="#F1F181";
+    }
+    
 }
 
 function updateCounter(op){
@@ -26,8 +33,10 @@ function updateCounter(op){
     }else {
         newValue = subtractNumber(number);
     };
+    changeColor(newValue, displayCounter);
     displayCounter.textContent= newValue;
 }
+
 
 
 //DOM
@@ -36,52 +45,11 @@ const displayCounter = document.getElementById("value-counter");
 const btnContainer = document.querySelector(".btn-container");
 const plus = document.getElementById("plus-btn");
 const minus = document.getElementById("minus-btn");
+const reloadBtn = document.getElementById("reload-btn");
+
 
 
 // Event
-
-
-// plus.addEventListener("click", ()=>{
-//     let number = getDisplay(displayCounter);
-//     newValue = addNumber(number);
-//     displayCounter.textContent= newValue;
-//     console.log(newValue);
-// })
-// minus.addEventListener("click", ()=>{
-//     let number = getDisplay(displayCounter);
-//     newValue = subtractNumber(number);
-//     displayCounter.textContent= newValue;
-//     console.log(newValue);
-// })
-
-
-
-//Scrivo il procedimento: 1. prendi il numero che segna il display, 2 riconosci il tipo di bottone premuto, 3 aggiungi un evento al bottone selezionato, 4 applica la funzione di
-//addizione o sottrazione 5. cambia il numero sul display ---> problema 2 ho un container che ha due bottoni come faccio a capire quale dei due ho premuto ?
-
-
-
-// btnContainer.addEventListener("click", (e)=>{
-//     let operation = e.target.closest("button");
-//     if (operation!=null){
-//         if(operation===plus){
-//             let number = getDisplay(displayCounter);
-//             newValue = addNumber(number);
-//             displayCounter.textContent= newValue;             
-//         }else{
-//             let number = getDisplay(displayCounter);
-//             newValue = subtractNumber(number);
-//             displayCounter.textContent= newValue;
-//         };
-//     };
-
-// }) 
-
-
-    
-
-
-
 
 btnContainer.addEventListener("click", (e)=>{
     let operation = e.target.closest("button");
@@ -89,5 +57,14 @@ btnContainer.addEventListener("click", (e)=>{
         updateCounter(operation);
     };
 }) 
+
+reloadBtn.addEventListener("click", ()=>{
+    displayCounter.textContent = 0;
+    displayCounter.style.color = "White";
+})
+
+
+
+
 
 
